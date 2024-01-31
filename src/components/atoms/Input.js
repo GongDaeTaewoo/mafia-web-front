31351c4f36523b0ba76a20056f17acacdb11e8ab
taewoo <dlscksdlsi2@gmail.com>
2024-01-31 17:01:34 +0900

    input창 구현

---
+++ src/components/atoms/Input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Input({ variant }) {
+  switch (variant) {
+    case 'long':
+      return (
+        <input
+          className="form-control bg-dark-subtle"
+          style={{ width: '800px' }}
+        />
+      );
+    case 'short':
+      return (
+        <input
+          className="form-control bg-dark-subtle"
+          style={{ width: '500px' }}
+        />
+      );
+    case 'forMyPage':
+      return <input className="form-control" style={{ width: '300px' }} />;
+    default:
+      return (
+        <input
+          className="form-control bg-dark-subtle"
+          style={{ width: '500px' }}
+        />
+      );
+  }
+}
+
+Input.propTypes = {
+  variant: PropTypes.string.isRequired,
+};
+
+export default Input;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Text from '../components/atoms/Text';
-import Button from '../components/atoms/button';
 
 function MainPage() {
   return (
     <div>
       MainPage
       <Text>aaa</Text>
-      <Button variant="register" />
     </div>
   );
 }
