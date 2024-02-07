4083f50f55e8cdbf2994026187a79bfa98bd7eb2
kjh <akkaa1119@naver.com>
2024-02-07 17:06:26 +0900

    refactor : 로그인 이미지 추가 및 이름 수정

--- src/components/organisms/LoginView.js
+++ src/components/organisms/LoginView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css as emotionCss } from '@emotion/react';
 import ImageView from '../atoms/ImageView';
-import loginImage from '../../assets/images/login.svg';
+import loginImage from '../../assets/images/mafia_login.svg';
 import LoginInput from '../molecules/LoginInput';
 import logoImage from '../../assets/images/mafia_logo.svg';
 import SocialLogin from '../molecules/SocialLogin';
