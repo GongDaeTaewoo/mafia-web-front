4aea56104e450e40a230f63c852b123569e6c20e
amanecopse <amanecopse@gmail.com>
2024-03-31 14:44:42 +0900

    refactor: signup API 주소 수정

--- src/components/molecules/SignupInput.js
+++ src/components/molecules/SignupInput.js
@@ -8,26 +8,25 @@ import Text from '../atoms/Text';
 import theme from '../../styles/theme';
 import Button from '../atoms/Button';
 
-
 /** @jsxImportSource @emotion/react */
 
 function SignupInput({ css }) {
   const [email, setEmail] = useState('');
   const [emailIsDisabled, setEmailIsDisabled] = useState(false);
-  const [emailBgColor,setEmailBgColor] = useState('');
+  const [emailBgColor, setEmailBgColor] = useState('');
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [showEmailButton, setShowEmailButton] = useState(true);
-  const [emailExist,setEmailExist] = useState(false);
+  const [emailExist, setEmailExist] = useState(false);
 
-  const [showEmailCodeButton,setShowEmailCodeButton] = useState(false);
-  const [showEmailCodeField,setShowEmailCodeField] = useState(false);
+  const [showEmailCodeButton, setShowEmailCodeButton] = useState(false);
+  const [showEmailCodeField, setShowEmailCodeField] = useState(false);
   const [showAdditionalFields, setShowAdditionalFields] = useState(false);
 
-  const [emailBtnIsDisabled,setEmailBtnIsDisabled] = useState(false);
+  const [emailBtnIsDisabled, setEmailBtnIsDisabled] = useState(false);
 
   const [emailCode, setEmailCode] = useState('');
   const [emailCodeIsDisabled, setEmailCodeIsDisabled] = useState(false);
-  const [emailCodeBgColor,setEmailCodeBgColor] = useState('');
+  const [emailCodeBgColor, setEmailCodeBgColor] = useState('');
   const [emailCodeIsValid, setEmailCodeIsValid] = useState(true);
 
   const [password, setPassword] = useState('');
@@ -38,22 +37,21 @@ function SignupInput({ css }) {
   const [nickname, setNickname] = useState('');
 
   const navigate = useNavigate();
-  
+
   const handleConfirmEmailChange = (e) => {
     const newEmail = e.target.value;
     setEmail(newEmail);
     setEmailExist(false);
-    
+
     const emailRegex = /^[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+$/;
-    const isVaildEmail= emailRegex.test(newEmail);
+    const isVaildEmail = emailRegex.test(newEmail);
     setEmailIsValid(isVaildEmail);
-  }
-  
+  };
 
   const handleConfirmEmailCodeChange = (e) => {
     const newEmailCode = e.target.value;
     setEmailCode(newEmailCode);
-  }
+  };
 
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
@@ -75,248 +73,306 @@ function SignupInput({ css }) {
 
   const handleConfirmNicknameChange = (e) => {
     setNickname(e.target.value);
-  
   };
 
-
-  const handleSendEmail= () => {
-    
-    if(emailIsValid){
+  const handleSendEmail = () => {
+    if (emailIsValid) {
       setEmailBtnIsDisabled(true);
 
-      const apiUrl ='http://localhost:8081/member/send-mail';
-  
-    axios.post(apiUrl, {
-      email,
-    })
-      .then(()=> {
-        setEmailBgColor('bg-secondary');
-        setShowEmailButton(false);
-        setEmailIsDisabled(true);
-        setShowEmailCodeField(true);
-        setShowEmailCodeButton(true);
-      })
-      .catch(error => {
-        setEmailBtnIsDisabled(false);
-        if(error.code==='ERR_BAD_REQUEST'){
-          setEmailExist(true);
-        }
-        
-      });
+      const apiUrl = 'http://localhost:8081/member/send-mail';
+
+      axios
+        .post(apiUrl, {
+          email,
+        })
+        .then(() => {
+          setEmailBgColor('bg-secondary');
+          setShowEmailButton(false);
+          setEmailIsDisabled(true);
+          setShowEmailCodeField(true);
+          setShowEmailCodeButton(true);
+        })
+        .catch((error) => {
+          setEmailBtnIsDisabled(false);
+          if (error.code === 'ERR_BAD_REQUEST') {
+            setEmailExist(true);
+          }
+        });
     }
   };
 
-  const handleAdditionalFields= () => {
-    const apiUrl ='http://localhost:8081/member/confirm-mail';
+  const handleAdditionalFields = () => {
+    const apiUrl = 'http://localhost:8081/member/confirm-mail';
 
-    axios.post(apiUrl, {
-      email,
-      emailCode,
-    })
-      .then(response => {
-        if(response.data.body === true){ 
+    axios
+      .post(apiUrl, {
+        email,
+        emailCode,
+      })
+      .then((response) => {
+        if (response.data.body === true) {
           setEmailCodeBgColor('bg-secondary');
           setEmailCodeIsDisabled(true);
 
           setEmailCodeIsValid(true);
           setShowAdditionalFields(true);
           setShowEmailCodeButton(false);
-        }else{
+        } else {
           setEmailCodeIsValid(false);
         }
-      })
+      });
   };
 
-  const handleSignup = () =>{
-    const apiUrl='http://localhost:8081/member/sign-up';
+  const handleSignup = () => {
+    const apiUrl = 'http://localhost:8081/member/signup';
 
-    if((passwordIsValid&&passwordsMatch)&&nickname!==''){
-
-      axios.post(apiUrl,{
-        email,
-        emailCode,
-        password,
-        nickname
-      })
-      .then(()=> {
-
-        navigate('/login');
-        
-      })
-      
+    if (passwordIsValid && passwordsMatch && nickname !== '') {
+      axios
+        .post(apiUrl, {
+          email,
+          emailCode,
+          password,
+          nickname,
+        })
+        .then(() => {
+          navigate('/login');
+        });
     }
-    
-  }
-  
-  const bootstrapEmailClass = (emailIsValid||email==='') ? '' : 'is-invalid'
-  const bootstrapEmailCodeClass = (emailCodeIsValid) ? '' : 'is-invalid'
-  const bootstrapPwClass = (passwordIsValid||password==='') ? '' : 'is-invalid'
-  const bootstrapPwConfirmClass = (passwordsMatch||confirmPassword==='') ? '' : 'is-invalid'
+  };
 
+  const bootstrapEmailClass = emailIsValid || email === '' ? '' : 'is-invalid';
+  const bootstrapEmailCodeClass = emailCodeIsValid ? '' : 'is-invalid';
+  const bootstrapPwClass =
+    passwordIsValid || password === '' ? '' : 'is-invalid';
+  const bootstrapPwConfirmClass =
+    passwordsMatch || confirmPassword === '' ? '' : 'is-invalid';
 
   const containerCss = emotionCss(
     {
-      display: 'flex',  
+      display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: theme.color.MAFIA_CONTAINER,
-      padding: "6rem",
+      padding: '6rem',
       '@media (max-width: 600px)': {
         padding: '3rem',
-      }
+      },
     },
     css,
   );
 
-  const inputCss = emotionCss(
-    {
-      width: "400px",
-      height: "30px",
-      fontSize: "15px",
-      '@media (max-width: 600px)': {
-        width: "200px",
-        height:"20px",
-      }
-    }
-  )
+  const inputCss = emotionCss({
+    width: '400px',
+    height: '30px',
+    fontSize: '15px',
+    '@media (max-width: 600px)': {
+      width: '200px',
+      height: '20px',
+    },
+  });
 
-  const buttonCss = emotionCss(
-    {
-      width: "400px",
-      height: "35px",
-      marginTop: "25px",
-      '@media (max-width: 600px)': {
-        width: "200px",
-        height:"25px",
-      }
-    }
-  )
+  const buttonCss = emotionCss({
+    width: '400px',
+    height: '35px',
+    marginTop: '25px',
+    '@media (max-width: 600px)': {
+      width: '200px',
+      height: '25px',
+    },
+  });
 
-  const partCss = emotionCss(
-    {
-      marginTop: "1rem",
-    }
-  )
+  const partCss = emotionCss({
+    marginTop: '1rem',
+  });
 
   return (
-    <div css= {containerCss}>
-        <Text variant={theme.fontVariant.H5} color={theme.color.MAFIA_WHITE}>
-          회원가입
+    <div css={containerCss}>
+      <Text variant={theme.fontVariant.H5} color={theme.color.MAFIA_WHITE}>
+        회원가입
+      </Text>
+      <div css={partCss}>
+        <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
+          이메일
         </Text>
-        <div css={partCss}>
-            <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
-                이메일
+
+        <input
+          className={`form-control ${bootstrapEmailClass} ${emailBgColor}`}
+          value={email}
+          onChange={handleConfirmEmailChange}
+          css={inputCss}
+          style={{ borderWidth: '3.5px' }}
+          disabled={emailIsDisabled}
+        />
+      </div>
+
+      {!emailIsValid && email !== '' && !emailExist && (
+        <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
+          올바른 이메일 형식이 아닙니다.
+        </Text>
+      )}
+
+      {emailExist && (
+        <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
+          이미 회원가입된 이메일입니다..
+        </Text>
+      )}
+
+      {(emailIsValid || email === '') && !emailExist && (
+        <Text variant={theme.fontVariant.SMALL}>
+          <br />
+        </Text>
+      )}
+
+      {showEmailCodeField && (
+        <>
+          <div css={partCss}>
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_WHITE}
+            >
+              이메일 인증코드
             </Text>
 
-            <input className={`form-control ${bootstrapEmailClass} ${emailBgColor}`} value={email} onChange={handleConfirmEmailChange} css={inputCss} style={{ borderWidth: '3.5px'}} disabled={emailIsDisabled}/>
-        </div>
-
-        {!emailIsValid&&email!=='' &&!emailExist&&(
-          <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
-            올바른 이메일 형식이 아닙니다.
-          </Text>
-        )}
-
-        {emailExist&&(
-          <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
-            이미 회원가입된 이메일입니다..
-          </Text>
-        )}
-
-        {(emailIsValid||email==='') && !emailExist && (
-          <Text variant={theme.fontVariant.SMALL}>
-            <br/>
-          </Text>
-        )}
-
-
-        {showEmailCodeField &&(
-          <>
-          <div css={partCss}>
-              <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
-                  이메일 인증코드
-              </Text>
-
-              <input className={`form-control ${bootstrapEmailCodeClass} ${emailCodeBgColor}`} value={emailCode} onChange={handleConfirmEmailCodeChange} css={inputCss} style={{ borderWidth: '3.5px'}} disabled={emailCodeIsDisabled}/>
-            
+            <input
+              className={`form-control ${bootstrapEmailCodeClass} ${emailCodeBgColor}`}
+              value={emailCode}
+              onChange={handleConfirmEmailCodeChange}
+              css={inputCss}
+              style={{ borderWidth: '3.5px' }}
+              disabled={emailCodeIsDisabled}
+            />
           </div>
-          {(!emailCodeIsValid)  && (
-            <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
+          {!emailCodeIsValid && (
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_RED}
+            >
               인증번호가 일치하지 않습니다.
             </Text>
           )}
 
-          {(emailCodeIsValid)  && (
-            <br/>
-          )}
-    
-          </>
-        
-        )}
+          {emailCodeIsValid && <br />}
+        </>
+      )}
 
-        {showAdditionalFields &&(
+      {showAdditionalFields && (
         <>
+          <div css={partCss}>
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_WHITE}
+            >
+              닉네임
+            </Text>
+            <input
+              value={nickname}
+              className={`form-control `}
+              onChange={handleConfirmNicknameChange}
+              css={inputCss}
+              style={{ borderWidth: '3.5px' }}
+            />
+            <br />
+          </div>
 
           <div css={partCss}>
-              <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
-                  닉네임
-              </Text>
-              <input value={nickname} className={`form-control `} onChange={handleConfirmNicknameChange} css={inputCss} style={{ borderWidth: '3.5px'}}/>
-              <br/>
-          </div>
-        
-          <div css={partCss}>
-              <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
-                  비밀번호
-              </Text>
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_WHITE}
+            >
+              비밀번호
+            </Text>
 
-              <input type="password" className={`form-control ${bootstrapPwClass}`} value={password} onChange={handlePasswordChange} css={inputCss} style={{ borderWidth: '3.5px'}}/>
+            <input
+              type="password"
+              className={`form-control ${bootstrapPwClass}`}
+              value={password}
+              onChange={handlePasswordChange}
+              css={inputCss}
+              style={{ borderWidth: '3.5px' }}
+            />
           </div>
 
-          {(!passwordIsValid&&password!=='')   && (
-            <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
+          {!passwordIsValid && password !== '' && (
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_RED}
+            >
               올바른 비밀번호 형식이 아닙니다.
             </Text>
           )}
 
-          {(passwordIsValid||password==='') && (
+          {(passwordIsValid || password === '') && (
             <Text variant={theme.fontVariant.SMALL}>
               <br />
             </Text>
           )}
 
           <div css={partCss}>
-              <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_WHITE}>
-                  비밀번호 확인
-              </Text>
-              <input type="password" className={`form-control ${bootstrapPwConfirmClass}`} value={confirmPassword} onChange={handleConfirmPasswordChange} css={inputCss} style={{ borderWidth: '3.5px'}}/>
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_WHITE}
+            >
+              비밀번호 확인
+            </Text>
+            <input
+              type="password"
+              className={`form-control ${bootstrapPwConfirmClass}`}
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              css={inputCss}
+              style={{ borderWidth: '3.5px' }}
+            />
           </div>
 
-          {(!passwordsMatch&&confirmPassword!=='') && (
-            <Text variant={theme.fontVariant.SMALL} color={theme.color.MAFIA_RED}>
+          {!passwordsMatch && confirmPassword !== '' && (
+            <Text
+              variant={theme.fontVariant.SMALL}
+              color={theme.color.MAFIA_RED}
+            >
               비밀번호가 일치하지 않습니다.
             </Text>
           )}
 
-          {(passwordsMatch||confirmPassword==='') && (
+          {(passwordsMatch || confirmPassword === '') && (
             <Text variant={theme.fontVariant.SMALL}>
               <br />
             </Text>
           )}
 
-      
-        <Button type="button" variant={theme.buttonVariant.REGIS} className="btn btn-secondary" css={buttonCss} onClick={handleSignup}>회원가입</Button>
-      </>)}
+          <Button
+            type="button"
+            variant={theme.buttonVariant.REGIS}
+            className="btn btn-secondary"
+            css={buttonCss}
+            onClick={handleSignup}
+          >
+            회원가입
+          </Button>
+        </>
+      )}
 
-    {showEmailButton && (
-        <Button type="button" variant={theme.buttonVariant.REGIS} className="btn btn-secondary" css={buttonCss} onClick={handleSendEmail} disabled={emailBtnIsDisabled}>
+      {showEmailButton && (
+        <Button
+          type="button"
+          variant={theme.buttonVariant.REGIS}
+          className="btn btn-secondary"
+          css={buttonCss}
+          onClick={handleSendEmail}
+          disabled={emailBtnIsDisabled}
+        >
           이메일 전송
         </Button>
       )}
 
-    {showEmailCodeButton && (
-        <Button type="button" variant={theme.buttonVariant.REGIS} className="btn btn-secondary" css={buttonCss} onClick={handleAdditionalFields}>
+      {showEmailCodeButton && (
+        <Button
+          type="button"
+          variant={theme.buttonVariant.REGIS}
+          className="btn btn-secondary"
+          css={buttonCss}
+          onClick={handleAdditionalFields}
+        >
           인증코드 확인
         </Button>
       )}
