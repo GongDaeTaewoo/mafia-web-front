3e8483531767fb64fc94a2a138638c3052daf74a
taewoo <yun68000@naver.comgit config --global user.emaigit config --global user.email yun68000@naver.com>
2024-04-01 01:43:56 +0900

    feat: 서버에서 받은것 search페이지에 뿌려줌,오류있음

--- src/axios/apis/profile.js
+++ src/axios/apis/profile.js
@@ -3,5 +3,8 @@ import authInstance from '../utils/authInstance';
 export const getProfileByUserId = async (userId) =>
   authInstance.get(`/member/users/${userId}/profile`);
 
+export const getProfilesByPage = async (page) =>
+  authInstance.get(`/member/${page}/`);
+
 export const patchProfile = async (userId, data) =>
   authInstance.patch(`/member/users/${userId}/profile`, data);
